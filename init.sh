7c865a89f38d1d41f7cd77869b52f8d6012c5a38
Maksym Diabin <maksym.diabin@gmail.com>
2017-08-18 13:40:18 +0100

    Added zsh as the default shell

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 sudo apt-get update
-sudo apt-get install vim
+sudo apt-get -y install vim clojure nodejs python zsh git-core 
+
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+chsh -s `which zsh`
 
 git config --global user.name "Maksym Diabin"
 
